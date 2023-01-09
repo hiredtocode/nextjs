d9b86911b2868604f650d3509a188b803f2f0b91
hiredtocode <jasonhan@kakao.com>
2023-01-10 00:35:03 +0900

    getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -11,12 +11,29 @@ function Post({ post }) {
 
 export default Post
 
+export async function getStaticPaths() {
+	return {
+		paths: [
+			{
+				params: { postId: '1' },
+			},
+			{
+				params: { postId: '2' },
+			},
+			{
+				params: { postId: '3' },
+			},
+		],
+		fallback: false,
+	}
+}
+
 export async function getStaticProps(context) {
 	const { params } = context
 	const response = await fetch(
 		`https://jsonplaceholder.typicode.com/posts/${params.postId}`
 	)
-	const data = await response.json
+	const data = await response.json()
 
 	return {
 		props: {
