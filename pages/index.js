ffd4d547c193b5eb270aadb7cb2725b04ac982f9
hiredtocode <jasonhan@kakao.com>
2023-01-09 15:28:49 +0900

    Navigating programatically

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,21 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+	const router = useRouter()
+
+	const onPlaceOrder = () => {
+		console.log(`Placing your order`)
+		router.push('/product')
+	}
+
 	return (
 		<div>
 			<h1>Home Page</h1>
 
 			<Link href='/blog'>Blog</Link>
 			<Link href='/product'>Products</Link>
+			<button onClick={onPlaceOrder}>Place order</button>
 		</div>
 	)
 }
